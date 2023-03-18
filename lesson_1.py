3e4f328462fe88d0125d7578b8e86f7049ca5c9f
Arseniy <gotansoff13@gmail.com>
2023-03-18 17:35:14 +0900

    Lesson_17 HomeWork

---
+++ lesson_1.py
@@ -0,0 +1,349 @@
+# # number - float(input('Number: '))
+
+# # if number % 1 == 0
+# #     print('Integer')
+# # else
+# #     print('Float')
+
+
+# # number = int(input("Number: "))
+
+# # if number % 5 == 0 or number % 10 == 0:
+# #     print("Ваше число ділиться на 5 або на 10")
+# # else:
+# #     print("Ваше число не ділиться на 5 або на 10")
+# # #
+
+
+# # a = float(input('Enter 1st slide: '))
+# # b = float(input('Enter 2nd slide: '))
+
+# # if a == b
+# #    print('this is square')
+# # else
+# #     measurement = int(input('What do you want to measure? ( 1 - area, 2 - perimeter)'))
+# #     if measurement == 1 :
+# #         print(f'Area is {a*b}')
+# #     elif measurement == 2
+# #         print(f"Perimetr is {2")
+        
+        
+# #  month = int(input("Enter month when u borth: "))
+# #  day = int(input("Enter day when u borth:"))
+ 
+# # if month > 12 and day > 31:
+# #     print ("Введите правду")
+# # else:
+# #     print("Ok")
+
+# # month = int(input('Month: '))
+# # day = int(input('Day: '))
+# # print('Ok') if month <= 12 and day <= 31 else print ( ' Wrong input!')
+ 
+     
+# # number - fl
+
+# # a = int(input('a: '))
+# # b = int(input('b: '))
+# # c = int(input('c: '))
+
+# # if a + b + c == 180:
+# #     print('Ok')
+# # else:
+# #     print('Не валідний')
+
+# # number = [int(i) for i in input(). split()]
+
+# # for number in numbers:
+# #     int(numbers)
+
+# # print = (numbers)
+
+
+# # print(type(numbers))
+
+# # for i in range(0, 7+1, 2):
+# #     print(i, end=' ')
+
+# # max - int(input('The size of the table: '))
+
+# # for row in range(1, max+1):
+# #    for column in range(1, max+1):
+# #        print(column*row, end='/e')
+# #        4 
+
+# # a = 1
+# # print(a)
+# # while a % 3 == 0:
+# #     a += 1
+# #     print(a)
+
+# # while True:
+# #     name = input('Enter name: ')
+# #     if name == 'stop'
+# #        print('Stopped')
+# #        break
+# #    print('Hello', name)
+
+# # a=0
+# # while a < 6 :
+# #      a = a + 1
+# # #      if not a % 2:
+# # #          continue
+# # #      print(a)
+
+# # string = input('Enter your string: ')
+# # first_chat = string[0]
+# # count = 0
+# # for chat in string:
+# #     if char == first_char:
+# #         count += 1
+
+# # # print(f'Amount of the first letter {first_chat} is {count}')
+
+# # print('Материки - частини Європи, розташовані на захід від Лондона, частини Африки, Атлантичний океан, Америка та більша частина Тихого океану..')
+
+# # west = ('South America','North America', 'Antactica')
+
+# # west.axtend(['Eurasia', 'Africa', 'Australia'])
+
+# # west.sort()
+
+# # print(west)
+
+
+# a = 'Arseniy Ykachenko'
+
+# first_element_of_surname = a[0]
+# first_elemnt_of_name = a[-4]
+
+# if fir
+
+# s = 'absdefgijk'
+# s[-1 :-6: -2]
+
+# print(s[3:6])
+
+# print(s[:6])
+
+# print(s[3:])
+
+# print(s[::-1])
+
+# is_palindroma = 'otto'
+
+# if is_palindorome == is_palindrome[::-1]
+#       print(True)
+# else
+#        print(false)
+
+# a = 'Jingle bells, jingle bells '
+
+# b = 'Jingle all the way '
+
+# c = 'Oh, what fun it is to ride'
+
+# d = 'In a one horse open sleigh'
+
+# print(a+b+c+d)
+
+# delimeter = '-' * 25
+# print(len(delimeter))
+
+# a = 'Jingle bells, jingle bells Jingle all the way Oh, what fun it is to ride In a one horse open sleigh'
+
+# for char in a:
+#     print(char)
+
+# s = "Hi there!"
+# start - 0
+# end = 5
+
+# print(s.find('er', 0, 7))
+# print(s.find('і'))
+
+
+# e = 'Big, Bigger, Biggest'
+# x = e.rfind('Big')
+# print(x)
+
+# stop_words -['купити','продати','реклама']
+
+# use_string = input()
+
+# for stop_word in stop_words:
+#     if user_string.find(stop_word) != -1:
+#      break
+# else:
+#     print('It is not spam')
+
+# if n > len(my_string):
+#     print('Wrong input!')
+# else:
+#     new_string = my_string[n:]
+#     print(new_string)
+
+# s= "i am learning strings in Pythom. Some new methods got now."
+# sentences = s.split('. ')
+# sentences.pop(-1)
+
+# print(sentences)
+
+# map = {
+#     ord('з'): 'z'
+# }
+
+# translated = 'з' .translate(map)
+
+# print(translated)
+
+# print ('pi: {:0.6}'.format(3.1415.926))
+
+# print('"{}""{:+})
+
+# delimiter = '-' * 80
+# goods = [['А', 1, 1], ['Р', 1, 1], ['С', 1, 1], ['Е', 1, 1], ['Н', 1, 1], ['И', 1, 1], ['Й', 1, 1]]
+# total_sum = 0
+# number = 0
+
+# print(delimiter)
+# print("|{:^5}|{:<40}|{:>15}| {:>14}|".format('#', 'Имья', "Кількість", "Сколько всего букв"))
+# print(delimiter)
+
+# for good in goods:
+#     name = good[0]
+#     amount = good[1]
+#     money = good[2]
+#     number += 1
+#     total_sum += money
+#     print("|{:^5}|{:<40}|{:>15}|{:>15}|".format(number, name, amount, money))
+
+# print(delimiter)
+# print("|{:<62}|{:>15}|".format(' Total:', total_sum))
+# print(delimiter)
+
+# a = '1 2 3 4 5 6 7 8 9 0'
+
+# for char in a:
+#      print(char)
+
+
+# 25331 => [1,3,2,5,3]
+
+# 0 => [0]
+
+# currencies = {
+#     'USD' : 36.42
+#     'EUR' : 39.03
+#     'GBP' : 43.84
+# }
+
+# currencies['PLN'] = 8.18
+# currencies['USD'] = 39
+# currencies['EUR'] = 41
+# currencies['GBP'] = 5.3
+
+# currency
+
+# for good in goods:
+#     name = good[0]
+#     amount = good[1]
+#     money = good[2]
+#     number += 1
+#     total_sum += money
+#     print("|{:^5}|{:<40}|{:>15}|{:>15}|".format(number, name, amount, money))
+ 
+# mylist = ('ab', 'cd', 'ef')## list
+ 
+# i=0 ##Define empty index value
+# for val in mylist:
+#      print(f'{i}: {val}')
+#      i +=1 
+    
+# print(f'what happen?')
+# for vale in print:
+#      print(f'i am worked with me father')
+
+# print  (('I like Pyton'))
+# a = input ()
+# print(a. upper())
+# print(a. lower())
+
+# surname = input('Enter your surname: ')
+# counter = dict()
+
+# for char in surname:
+#     counter[char] = surname.count(char)
+    
+# print(counter.items())
+# print(list(counter.items()))
+# most_common = max(list(counter.items()), key=lambda x: x[1])
+    
+# most_common = max(list(counter.items()), key=lambda x: x[1])
+# print(most_common[0])
+
+# string = input() + ' '
+
+# space = string.find(' ')
+
+# word = string[:space]
+# print(word)
+# new_string = string[space+1:]
+# print(new_string)
+
+# # print(string.count(' '))
+# for i in range(string.count(' ')):
+#     space = new_string.find(' ')
+#     if len(new_string[:space]) >= len(word):
+#         word = new_string[:space]
+#     new_string = new_string[space+1:]
+    
+# print(f'The longest word is {word}')
+
+# grades = {
+#     "Artem": 2
+#     "Anna": 5
+#     "Dmytro": 3
+#     "Andriy": 4
+# }
+
+# good_grades = dict()
+
+# for student_name, grade in grades.items():
+#     if grade >= 4:
+#         good_grades[student_name] = grade
+        
+# print(good_grades)
+
+# forecast = {
+#     "Kyiv": 25,
+#     "Cherkasy": 27,
+#     "Odessa": 30,
+#     "donetsk":26
+# }
+
+# total_temperature = 0
+# count = 0
+
+# for temp in forecast.values():
+#     total_temperature += temp
+#     count += 1
+    
+# print(f'Avg of temperatures = {tota;_temperature/count}')
+
+# text = 'Lorem impusum dolor sit amet'
+
+# chars_counter = dict()
+# {element: count}
+# for char in text:
+#     if char != ' ':
+#         if char not in chars_counter:
+#             chars_counter[char] = text.count(char)
+            
+# print(chars_counter)
+# counter_list = sorted(list(chars_counter.items()), key=lambda x: x[1])
+# counter_list.reverse()
+# print(counter_list)
+
+
+    
---
+++ lesson_2.py
@@ -0,0 +1,18 @@
+# booking_book = {}
+
+# def booking(clients_name, amount=2):
+#     booking_book[client_name] = amount
+#     print(f'Client {clients_name}, booked {amount}-places table.')
+    
+    
+# booking('Arseniy')
+# booking('Andriy', 4)
+
+# print(booking_book)
+
+def function(list):
+    a = len(list)
+    b = sum(list)
+    return b/a
+
+print(function([1,3,6,9,12,15,18]))
